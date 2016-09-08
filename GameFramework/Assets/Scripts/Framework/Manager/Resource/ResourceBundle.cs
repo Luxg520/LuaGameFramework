cc516ddb479d4b4518108045578d44ffeb02ad55
Luxg520 <luxg1314@163.com>
2016-09-09 00:10:34 +0800

    增加AssetBundle加载逻辑

--- GameFramework/Assets/Scripts/Framework/Manager/Resource/ResourceBundle.cs
+++ GameFramework/Assets/Scripts/Framework/Manager/Resource/ResourceBundle.cs
@@ -8,7 +8,9 @@ using System.Collections.Generic;
 public class ResourceBundle : ManagerBase<ResourceBundle>
 {
     // 已加载资源缓存
-    Dictionary<string, ResourceInfo> loadedResources = new Dictionary<string, ResourceInfo>();
+    private Dictionary<string, ResourceInfo> loadedResources = new Dictionary<string, ResourceInfo>();
+
+    #region 外部接口
 
     /// <summary>
     /// 同步加载并实例化Object
@@ -17,7 +19,7 @@ public class ResourceBundle : ManagerBase<ResourceBundle>
     /// <returns>GameObject</returns>
     public GameObject Instantiate(string path)
     {
-        GameObject go = Instantiate(Load(path));    
+        GameObject go = Instantiate(Load(path));
         return go;
     }
 
@@ -44,34 +46,13 @@ public class ResourceBundle : ManagerBase<ResourceBundle>
     }
 
     /// <summary>
-    /// 同步加载资源内部函数
-    /// </summary>
-    /// <param name="path">资源路径</param>
-    private UnityEngine.Object LoadInternal(string path)
-    {
-        
-        return AssetBundle.CreateFromFile(path).mainAsset;
-    }
-
-    /// <summary>
     /// 异步加载资源
     /// </summary>
     /// <param name="path"></param>
     /// <param name="cb"></param>
     public void LoadAsync(string path, Action<UnityEngine.Object> cb)
     {
-        LoadAsyncInternal(path, cb);
-    }
-
-    /// <summary>
-    /// 异步加载资源内部
-    /// </summary>
-    /// <param name="path"></param>
-    /// <param name="cb"></param>
-    private void LoadAsyncInternal(string path, Action<UnityEngine.Object> cb)
-    {
-        if (cb != null)
-            cb(UnityEditor.AssetDatabase.LoadMainAssetAtPath(path));
+        StartCoroutine(LoadAsyncInternal(path, cb));
     }
 
     /// <summary>
@@ -84,15 +65,80 @@ public class ResourceBundle : ManagerBase<ResourceBundle>
         StartCoroutine(LoadCoroutineInternal(path, cb));
     }
 
-    /// <summary>
-    /// 协程加载资源内部
-    /// </summary>
-    /// <param name="path"></param>
-    /// <param name="cb"></param>
+    #endregion
+
+    #region 资源管理内部函数
+
+    // 初始化
+    protected override void Init()
+    {
+        base.Init();
+
+    }
+
+    // 创建一个资源对象
+    private ResourceInfo CreateResource(string path)
+    {
+        ResourceInfo res = new ResourceInfo();
+        res.path = path;
+        return res;
+    }
+
+    // 获取一个资源对象
+    private ResourceInfo GetResource(string path)
+    {
+        ResourceInfo res = null;
+        loadedResources.TryGetValue(path, out res);
+        return res;
+    }
+
+    // 创建常驻资源
+    private void CreateResidentResources()
+    {
+        Debug.Log("创建常驻资源");
+    }
+
+    // 同步加载资源内部函数
+    private UnityEngine.Object LoadInternal(string path)
+    {
+
+        return AssetBundle.LoadFromFile(path);
+    }
+
+    // 异步加载资源内部
+    private IEnumerator LoadAsyncInternal(string path, Action<UnityEngine.Object> cb)
+    {
+        AssetBundleCreateRequest createRequest = AssetBundle.LoadFromFileAsync(path);
+        yield return createRequest;
+        AssetBundle assetBundle = createRequest.assetBundle;
+        if (assetBundle == null)
+        {
+            Debug.LogError("Failed to load AssetBundle");
+            yield break;
+        }
+
+        ResourceInfo resInfo = new ResourceInfo();
+        resInfo.path = path;
+        resInfo.assetBundle = assetBundle;
+
+        AssetBundleRequest loadRequest = resInfo.assetBundle.LoadAssetAsync<GameObject>(path);
+        yield return loadRequest;
+        resInfo.mainObj = loadRequest.asset;
+
+        // 加入缓存
+        loadedResources.Add(path, resInfo);
+
+        if (cb != null)
+            cb(loadRequest.asset);
+    }
+
+    // 协程加载资源内部
     private IEnumerator LoadCoroutineInternal(string path, Action<UnityEngine.Object> cb)
     {
         yield return null;
         if (cb != null)
             cb(UnityEditor.AssetDatabase.LoadMainAssetAtPath(path));
     }
+
+    #endregion
 }
