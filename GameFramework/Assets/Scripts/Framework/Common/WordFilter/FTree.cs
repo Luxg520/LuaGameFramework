c409d9df9692a3796ec3fe8db5902ff047f4355d
Luxg520 <luxg1314@163.com>
2016-10-19 15:44:33 +0800

    加入屏蔽字过滤组件

---
+++ GameFramework/Assets/Scripts/Framework/Common/WordFilter/FTree.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class FTreeHelper
+{
+    public static FNode BinaryInsert(ushort key, List<FNode> childs)
+    {
+        FNode node = null;
+        if (childs.Count == 0)
+        {
+            node = new FNode(key);
+            childs.Add(node);
+            return node;
+        }
+
+        int l = 0;
+        int r = childs.Count - 1;
+        int m = childs.Count / 2;
+
+        while (l <= r)
+        {
+            if (key > childs[m].key) l = m + 1;
+            else if (key < childs[m].key) r = m - 1;
+            else return childs[m];
+
+            m = (l + r) / 2;
+        }
+
+        node = new FNode(key);
+        if (key > childs[m].key)
+            childs.Insert(m + 1, node);
+        else
+            childs.Insert(m, node);
+
+        return node;
+    }
+
+    public static FNode BinarySearch(int key, List<FNode> childs)
+    {
+        if (childs.Count == 0)
+            return null;
+
+        int l = 0;
+        int r = childs.Count - 1;
+        int m = childs.Count / 2;
+
+        while (l <= r)
+        {
+            if (key > childs[m].key) l = m + 1;
+            else if (key < childs[m].key) r = m - 1;
+            else return childs[m];
+
+            m = (l + r) / 2;
+        }
+
+        return null;
+    }
+}
+
+public class FNode
+{
+    public List<FNode> childs = null;
+    public ushort key;
+    public bool terminated; // 是否找到数据
+
+    public FNode(ushort key)
+    {
+        this.key = key;
+        this.terminated = false;
+    }
+
+    public FNode InsertChild(ushort key)
+    {
+        if (childs == null)
+            childs = new List<FNode>();
+
+        return FTreeHelper.BinaryInsert(key, childs);
+    }
+
+    public FNode BinarySearch(ushort key)
+    {
+        if (childs == null)
+            return null;
+
+        return FTreeHelper.BinarySearch(key, childs);
+    }
+}
+
+public class FTree
+{
+    public List<FNode> childs = new List<FNode>();
+
+    public void InsertWord(string word)
+    {
+        if (string.IsNullOrEmpty(word))
+            return;
+
+        char[] chs = word.ToCharArray();
+
+        int index = 0;
+        FNode nextNode = null;
+        while (index < chs.Length)
+        {
+            ushort key = (ushort)chs[index++];
+
+            if (nextNode == null)
+                nextNode = FTreeHelper.BinaryInsert(key, childs);
+            else
+                nextNode = nextNode.InsertChild(key);
+        }
+
+        nextNode.terminated = true;
+    }
+
+    public bool Validate(string word)
+    {
+        if (string.IsNullOrEmpty(word))
+            return true;
+
+        char[] chs = word.ToCharArray();
+
+        int index = 0;
+        while (index < chs.Length)
+        {
+            int newIndex = 0;
+            if (Process(chs, index, ref newIndex))
+                return false;
+            else
+                index++;
+        }
+
+        return true;
+    }
+
+    public string Process(string word)
+    {
+        if (string.IsNullOrEmpty(word))
+            return "";
+
+        char[] chs = word.ToCharArray();
+
+        int index = 0;
+        while (index < chs.Length)
+        {
+            int newIndex = 0;
+            if (Process(chs, index, ref newIndex))
+                while (index < newIndex)
+                    chs[index++] = '*';
+            else
+                index++;
+        }
+
+        return new string(chs);
+    }
+
+    public bool Process(char[] chs, int offset, ref int result)
+    {
+        FNode nextNode = null;
+        result = -1;
+
+        int index = offset;
+        while (index < chs.Length)
+        {
+            ushort key = (ushort)chs[index++];
+
+            if (nextNode == null)
+                nextNode = FTreeHelper.BinarySearch(key, childs);
+            else
+                nextNode = nextNode.BinarySearch(key);
+
+            if (nextNode == null)
+                break;
+
+            if (nextNode.terminated)
+                result = index;
+        }
+
+        return result != -1;
+    }
+}
---
+++ GameFramework/Assets/Scripts/Framework/Common/WordFilter/WordFilter.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Swift;
+
+public class WordFilter : Component
+{
+    bool banWordLoaded = false;
+    bool validCharacterLoaded = false;
+
+    HashSet<char> validChineseSet;
+    FTree commonFtree = new FTree();
+    FTree nameFTree = new FTree();
+
+    public virtual void Load()
+    {
+        LoadBanWords();
+    }
+
+    public Func<string, byte[]> BinaryLoader = (string path) => { return File.ReadAllBytes(path); };
+    public string ConfigPath = "config";
+    public void LoadBanWords()
+    {
+        if (banWordLoaded)
+            return;
+
+        banWordLoaded = true;
+        byte[] bytes = BinaryLoader(ConfigPath + "/ban_words.txt");
+        string text = Encoding.UTF8.GetString(bytes);
+        string[] words = text.Split("\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+        FTree tree = null;
+        foreach (string word in words)
+        {
+            string str = word.Trim();
+            if (string.IsNullOrEmpty(str))
+                continue;
+
+            if (str[0] == '[')
+            {
+                if (str == "[名字]")
+                    tree = nameFTree;
+                else
+                    tree = commonFtree;
+            }
+
+            if (tree != null)
+                tree.InsertWord(str);
+        }
+    }
+
+    protected virtual void LoadValidChinese()
+    {
+        if (validCharacterLoaded)
+            return;
+
+        validCharacterLoaded = true;        
+        byte[] bytes = BinaryLoader(ConfigPath + "/valid_chinese.txt");
+        string text = Encoding.UTF8.GetString(bytes);
+        validChineseSet = new HashSet<char>();
+        for (int i = 0; i < text.Length; ++i)
+            validChineseSet.Add(text[i]);
+    }
+
+    // 是否是有效字符
+//    public bool IsValidChar(char ch)
+//    {
+//        LoadValidChinese();
+//        return validChineseSet.Contains(ch);
+//    }
+
+    // 字符串中是否全部是有效字符
+//    public bool IsValidText(string text)
+//    {
+//        if (text == null)
+//            return false;
+//
+//        LoadValidChinese();
+//
+//        for (int i = 0; i < text.Length; ++i)
+//        {
+//            if (!IsValidChar(text[i]))
+//                return false;
+//        }
+//
+//        return true;
+//    }
+
+    // 过滤合法字符
+//    public string FiltrateInvalidText(string text)
+//    {
+//        if (string.IsNullOrEmpty(text))
+//            return string.Empty;
+//
+//        StringBuilder sb = new StringBuilder();
+//        for (int i = 0; i < text.Length; ++i)
+//        {
+//            if (IsValidChar(text[i]))
+//                sb.Append(text[i]);
+//        }
+//
+//        return sb.ToString();
+//    }
+
+    // 过滤字符串的敏感词，并用*代替，返回过滤后的字符串
+    public string FilterSensitiveWord(string text)
+    {
+        return commonFtree.Process(text);
+    }
+
+    // 名字专用：名字是否包含敏感词或无效名字
+    public bool NameHasSensitiveWord(string text)
+    {
+        return !nameFTree.Validate(text) || !commonFtree.Validate(text);
+    }
+
+    // 名字以外使用：是否包含敏感词
+    public bool HasSensitiveWord(string text)
+    {
+        return !commonFtree.Validate(text);
+    }
+}
